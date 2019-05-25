c1f8c5bf43494586c579976c42836ed5086f3d53
christian plourde <christian.bertin.plourde@gmail.com>
2019-05-25 16:11:09 -0400

    initial commit

---
+++ App_Code/coulter_family_recipes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for coulter_family_recipes
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class coulter_family_recipes : System.Web.Services.WebService
+{
+
+    public coulter_family_recipes()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
---
+++ coulter_family_recipes.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/coulter_family_recipes.cs" Class="coulter_family_recipes" %>
